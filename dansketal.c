0337333d14c14babd334b6ac151f2df6d62cc325
JaDetGodtDu <123992392+JaDetGodtDu@users.noreply.github.com>
2024-09-09 14:55:16 +0200

    help. life is meaningless

--- dansketal.c
+++ dansketal.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "helpers.h"
 
-void dansketal(int num, char* buffer) {
+void dansketal(int num, char* buffer, DanskeTalConfig* config) {
     buffer[0] = '\0'; // Clear the buffer
 
     if (num < 0) {
@@ -13,21 +13,23 @@ void dansketal(int num, char* buffer) {
     if (num == 0) {
         strcat(buffer, "nul");
     } else if (num < 10) {
-        convert_units(num, buffer,0);
+        convert_units(num, buffer, 0, config);
     } else if (num < 100) {
-        convert_tens(num, buffer);
+        convert_tens(num, buffer, config);
     } else if (num < 1000) {
-        convert_hundreds(num, buffer);
+        convert_hundreds(num, buffer, config);
     } else if (num < 1000000) {
-        convert_thousands(num, buffer);
+        convert_thousands(num, buffer, config);
     } else if (num < 1000000000) {
-        convert_millions(num, buffer);
+        convert_millions(num, buffer, config);
     }
 }
+ 
 int main() {
     char buffer[256];
-    int number = 125000;
-    dansketal(number, buffer);
+    int number = 125;
+    DanskeTalConfig config = {1}; // Use "en" for large numbers
+    dansketal(number, buffer, &config);
     printf("%d in Danish is: %s\n", number, buffer);
     return 0;
 }
--- helpers.c
+++ helpers.c
@@ -1,83 +1,11 @@
-// #include <string.h>
-// #include "helpers.h"
-
-// void convert_units(int num, char* buffer) {
-//     const char* units[] = {"nul", "et", "to", "tre", "fire", "fem", "seks", "syv", "otte", "ni"};
-//     if (num >= 0 && num <= 9) {
-//         strcat(buffer, units[num]);
-//     }
-// }
-
-// void convert_tens(int num, char* buffer) {
-//     const char* tens[] = {"", "ti", "tyve", "tredive", "fyrre", "halvtreds", "tres", "halvfjerds", "firs", "halvfems"};
-//     if (num >= 10 && num <= 99) {
-//         if (num < 20) {
-//             const char* teens[] = {"ti", "elleve", "tolv", "tretten", "fjorten", "femten", "seksten", "sytten", "atten", "nitten"};
-//             strcat(buffer, teens[num - 10]);
-//         } else {
-//             if (num % 10 != 0) {
-//                 convert_units(num % 10, buffer);
-//                 strcat(buffer, "og");
-//             }
-//             strcat(buffer, tens[num / 10]);
-//         }
-//     }
-// }
-
-// void convert_hundreds(int num, char* buffer) {
-//     if (num >= 100 && num <= 999) {
-//         if (num / 100 == 1) {
-//             strcat(buffer, "et hundrede");
-//         } else {
-//             convert_units(num / 100, buffer);
-//             strcat(buffer, " hundrede");
-//         }
-//         if (num % 100 != 0) {
-//             strcat(buffer, " og ");
-//             convert_tens(num % 100, buffer);
-//         }
-//     }
-// }
-
-// void convert_thousands(int num, char* buffer) {
-//     if (num >= 1000 && num <= 999999) {
-//         int thousands = num / 1000;
-//         if (thousands < 10) {
-//             convert_units(thousands, buffer);
-//         } else if (thousands < 100) {
-//             convert_tens(thousands, buffer);
-//         } else {
-//             convert_hundreds(thousands, buffer);
-//         }
-//         strcat(buffer, " tusinde");
-//         if (num % 1000 != 0) {
-//             strcat(buffer, " ");
-//             convert_hundreds(num % 1000, buffer);
-//         }
-//     }
-// }
-
-// void convert_millions(int num, char* buffer) {
-//     if (num >= 1000000) {
-//         convert_units(num / 1000000, buffer);
-//         strcat(buffer, " million");
-//         if (num / 1000000 > 1) {
-//             strcat(buffer, "er");
-//         }
-//         if (num % 1000000 != 0) {
-//             strcat(buffer, " ");
-//             convert_thousands(num % 1000000, buffer);
-//         }
-//     }
-// }
 #include <string.h>
 #include "helpers.h"
 
-void convert_units(int num, char* buffer, int is_thousands) {
+void convert_units(int num, char* buffer, int is_large_number, DanskeTalConfig* config) {
     const char* units[] = {"nul", "et", "to", "tre", "fire", "fem", "seks", "syv", "otte", "ni"};
     const char* large_units[] = {"nul", "en", "to", "tre", "fire", "fem", "seks", "syv", "otte", "ni"};
     if (num >= 0 && num <= 9) {
-        if (is_thousands && num == 1) {
+        if (is_large_number && num == 1 && config->use_en_for_large_numbers) {
             strcat(buffer, large_units[num]);
         } else {
             strcat(buffer, units[num]);
@@ -85,7 +13,7 @@ void convert_units(int num, char* buffer, int is_thousands) {
     }
 }
 
-void convert_tens(int num, char* buffer) {
+void convert_tens(int num, char* buffer, DanskeTalConfig* config) {
     const char* tens[] = {"", "ti", "tyve", "tredive", "fyrre", "halvtreds", "tres", "halvfjerds", "firs", "halvfems"};
     if (num >= 10 && num <= 99) {
         if (num < 20) {
@@ -93,7 +21,7 @@ void convert_tens(int num, char* buffer) {
             strcat(buffer, teens[num - 10]);
         } else {
             if (num % 10 != 0) {
-                convert_units(num % 10, buffer, 0);
+                convert_units(num % 10, buffer, 0, config);
                 strcat(buffer, "og");
             }
             strcat(buffer, tens[num / 10]);
@@ -101,49 +29,56 @@ void convert_tens(int num, char* buffer) {
     }
 }
 
-void convert_hundreds(int num, char* buffer) {
+void convert_hundreds(int num, char* buffer, DanskeTalConfig* config) {
     if (num >= 100 && num <= 999) {
         if (num / 100 == 1) {
             strcat(buffer, "et hundrede");
         } else {
-            convert_units(num / 100, buffer, 0);
+            convert_units(num / 100, buffer, 0, config);
             strcat(buffer, " hundrede");
         }
         if (num % 100 != 0) {
             strcat(buffer, " og ");
-            convert_tens(num % 100, buffer);
+            convert_tens(num % 100, buffer, config);
         }
     }
 }
 
-void convert_thousands(int num, char* buffer) {
+void convert_thousands(int num, char* buffer, DanskeTalConfig* config) {
     if (num >= 1000 && num <= 999999) {
         int thousands = num / 1000;
         if (thousands < 10) {
-            convert_units(thousands, buffer, 1);
+            convert_units(thousands, buffer, 1, config);
         } else if (thousands < 100) {
-            convert_tens(thousands, buffer);
+            convert_tens(thousands, buffer, config);
         } else {
-            convert_hundreds(thousands, buffer);
+            convert_hundreds(thousands, buffer, config);
         }
         strcat(buffer, " tusinde");
         if (num % 1000 != 0) {
             strcat(buffer, " ");
-            convert_hundreds(num % 1000, buffer);
+            convert_hundreds(num % 1000, buffer, config);
         }
     }
 }
 
-void convert_millions(int num, char* buffer) {
+void convert_millions(int num, char* buffer, DanskeTalConfig* config) {
     if (num >= 1000000) {
-        convert_units(num / 1000000, buffer, 1);
+        int millions = num / 1000000;
+        if (millions < 10) {
+            convert_units(millions, buffer, 1, config);
+        } else if (millions < 100) {
+            convert_tens(millions, buffer, config);
+        } else {
+            convert_hundreds(millions, buffer, config);
+        }
         strcat(buffer, " million");
-        if (num / 1000000 > 1) {
+        if (millions > 1) {
             strcat(buffer, "er");
         }
         if (num % 1000000 != 0) {
             strcat(buffer, " ");
-            convert_thousands(num % 1000000, buffer);
+            convert_thousands(num % 1000000, buffer, config);
         }
     }
 }
